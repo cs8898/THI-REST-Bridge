351eeaa512047499c411d5f5f72389849cbffe7d
Christian Schmied <christian.schmied@geekinbusiness.de>
2019-12-06 21:02:19 +0100

    init

---
+++ src/test/java/ml/raketeufo/thi/restbride/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package ml.raketeufo.thi.restbride;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
