7944d5e686424c30bcd74a868019fd0790a93061
Christian Schmied <cs8898@gmx.de>
2020-07-07 22:34:33 +0200

    slight travis Fix, re added simple test

--- src/test/java/ml/raketeufo/thi/restbride/ExampleResourceTest.java
+++ src/test/java/ml/raketeufo/thi/restbride/ExampleResourceTest.java
@@ -1,23 +1,21 @@
 package ml.raketeufo.thi.restbride;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.containsStringIgnoringCase;
 
 @QuarkusTest
 public class ExampleResourceTest {
 
     @Test
-    @Disabled
-    public void testHelloEndpoint() {
+    public void testPingEndpoint() {
         given()
-                .when().get("/hello")
+                .when().get("/ping")
                 .then()
                 .statusCode(200)
-                .body(is("hello"));
+                .body(containsStringIgnoringCase("ok"));
     }
 
 }
